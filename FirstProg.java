65691074fe5a39f3b74e8be6608b1f02d76e3555
Erik Ernst <erikrobeyernst@Eriks-MacBook-Pro.local>
2017-07-28 09:40:13 -0500

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main ( String[] args ) {
+		System.out.println( "I, Erik Ernst, am determined to learn how to code." );
+		System.out.println( "Today's date is Friday, July 7, 2017" );
+	}
+}
